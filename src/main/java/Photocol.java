d2389b0e0eb981a1f624358ea46f160b68c2626c
Jonathan Lam <jlam55555@gmail.com>
2020-02-09 20:41:27 -0500

    added mvn junit deps and basic test case

---
+++ src/main/java/Photocol.java
@@ -0,0 +1,5 @@
+public class Photocol {
+    public static void main(String[] args) {
+        System.out.println("Hello, world from Photocol!");
+    }
+}
---
+++ src/test/java/PhotocolTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+public class PhotocolTest extends TestCase {
+    public void testPhotocol() throws Exception {
+        Photocol.main(new String[]{});
+
+        assertTrue(2 == 2);
+    }
+}

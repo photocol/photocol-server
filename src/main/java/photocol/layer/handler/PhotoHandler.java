79b20f7fd4662ed8c9effa47a201a808d16e5089
Jonathan Lam <jlam55555@gmail.com>
2020-02-29 01:27:40 -0500

    added some caching code

--- src/main/java/photocol/layer/handler/PhotoHandler.java
+++ src/main/java/photocol/layer/handler/PhotoHandler.java
@@ -20,10 +20,25 @@ public class PhotoHandler {
     }
 
     // simple image passthrough from s3
+    // todo: move this to service layer, add authentication
     public Object permalink(Request req, Response res) {
         String imageName = req.params("imageuri");
+        String conditionalHeader = req.headers("If-None-Match");
+        String eTag;
+
         ResponseInputStream<GetObjectResponse> response = s3.getObject(imageName);
+
+        // caching with etags
+        eTag = response.response().eTag();
+        if (conditionalHeader != null && conditionalHeader.equals(eTag)) {
+            res.status(304);
+            return 0;
+        }
         res.type(response.response().contentType());
+        res.header("Cache-Control", "public, max-age=3600");
+        res.header("ETag", response.response().eTag());
+
+        // return response stream
         return response;
     }
 }

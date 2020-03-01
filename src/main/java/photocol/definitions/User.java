d6463594832484186277f8b6b83441b36d310a7d
VICTOR <victorz.beijing@gmail.com>
2020-03-01 14:22:53 -0500

    User.java changed

--- src/main/java/photocol/definitions/User.java
+++ src/main/java/photocol/definitions/User.java
@@ -2,17 +2,20 @@ package photocol.definitions;
 
 public class User {
     private String username;
+    private String email;
     private String passwordHash;
 
-    public User(String username, String passwordHash) {
+
+    public User(String username,String email, String passwordHash) {
         this.username = username;
+        this.email = email;
         this.passwordHash = passwordHash;
     }
 
     public String getUsername() {
         return this.username;
     }
-
+    public String getEmail() { return this.email;}
     public boolean validatePasswordHash(String testHash) {
         return testHash.equals(passwordHash);
     }

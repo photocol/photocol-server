2acce4b15afb855e2069f864cc3bbbd88205479c
tiyu0203 <tiyu0203@gmail.com>
2020-03-01 13:19:29 -0500

    some code

--- src/main/java/photocol/layer/service/UserService.java
+++ src/main/java/photocol/layer/service/UserService.java
@@ -1,4 +1,24 @@
 package photocol.layer.service;
 
+import photocol.definitions.User;
+import photocol.definitions.response.StatusResponse;
+
 public class UserService {
+    public void checkUser() {
+       if signUp = 102
+            System.out.print("User Not found");
+       else
+           checkError()
+
+    }
+    public int signUp (User user){
+        if User.getUsername
+                return StatusResponse
+        elses
+            return StatusResponse
+    }
+
+    public int checkError() {
+        return 0;
+    }
 }

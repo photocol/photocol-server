20245f3fce953571d8a0f17188ede88ac37aa158
VICTOR <victorz.beijing@gmail.com>
2020-03-01 14:09:46 -0500

    TableManage done waiting database structure

--- src/main/java/photocol/DBTEST.java
+++ src/main/java/photocol/DBTEST.java
@@ -5,9 +5,11 @@ import photocol.layer.DataBase.Method.TableManage;
 import javax.swing.text.TabableView;
 import java.sql.*;
 public class DBTEST {
-    public static void main(String[] Args){
+    public static void main(String[] Args) throws SQLException {
         InitDB x = new InitDB();
         Connection passthrough = x.initialDB("data");
-
+        TableManage y = new TableManage(passthrough);
+        y.addTable("user", "name VARCHAR(20)", "email VARCHAR(50)", "password VARCHAR(20)");
+        y.addTable("collection", 2,"id INT NOT NULL","name VARCHAR(255) NOT NULL","user VARCHAR(255) NOT NULL");
     }
 }

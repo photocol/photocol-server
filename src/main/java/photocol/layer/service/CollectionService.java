1b95dcba436a33331483fbd4da87b9a50be4c9cb
tiyu0203 <tiyu0203@gmail.com>
2020-03-01 23:24:53 -0500

    photo and photocollection service beginning edits

--- src/main/java/photocol/layer/service/CollectionService.java
+++ src/main/java/photocol/layer/service/CollectionService.java
@@ -1,11 +1,17 @@
 package photocol.layer.service;
 
+import photocol.definitions.PhotoCollection;
 import photocol.layer.store.CollectionStore;
 
+import photocol.definitions.response.StatusResponse;
+//import photocol.layer.DataBase.PhotoCollectionDB;
+
+import static photocol.definitions.response.StatusResponse.Status.*;
+
 public class CollectionService {
-    public CollectionService(CollectionStore dummy){
+   /* private PhotoCollectionDB photocollectionDB
 
-
-    }
-
+    public CollectionService(PhotoCollectionDB photocollectionDB){
+        this.photocollectionDB = photocollectionDB;
+    }*/
 }
--- src/main/java/photocol/layer/service/PhotoService.java
+++ src/main/java/photocol/layer/service/PhotoService.java
@@ -1,9 +1,19 @@
 package photocol.layer.service;
 
+import photocol.definitions.Photo;
 import photocol.layer.store.PhotoStore;
 
-public class PhotoService {
-    public PhotoService(PhotoStore dummy){
+import photocol.definitions.response.StatusResponse;
+//import photocol.layer.DataBase.PhotoDB;
 
-    }
+import static photocol.definitions.response.StatusResponse.Status.*;
+
+
+public class PhotoService {/*
+    private PhotoDB photoDB;
+
+    public PhotoService(PhotoDB photoDB){
+        this.photoDB = photoDB;
+    }*/
+
 }

c4e3bc3d350537a56640c7bbb6f86c11a03d8a41
tiyu0203 <tiyu0203@gmail.com>
2020-03-01 19:03:43 -0500

    small changes

--- src/main/java/photocol/layer/service/UserService.java
+++ src/main/java/photocol/layer/service/UserService.java
@@ -12,20 +12,29 @@ import static photocol.definitions.response.StatusResponse.Status.*;
 public class UserService {
 
     private UserDB userDB;
-    public UserService(UserDB userDB){
+
+    public UserService(UserDB userDB) {
         this.userDB = userDB;
     }
-    public Status signUp(User user){
-        if (userDB.checkIfUserExists(user.email) == STATUS_USER_NOT_CREATED) {
-            //create user;
+
+    public Status signUp(User user) {
+        if (userDB.checkIfUserExists(user.email) == STATUS_USER_NOT_FOUND) {
+            //create users
+            if (userDB.signUp(user.email, user.username, user.passwordHash) == STATUS_USER_CREATED) {
+
+            } else if (userDB.signUp(user.email, user.username, user.passwordHash) == STATUS_USER_NOT_CREATED) {
+
+            }
 
             //TODO change the status code
-        }
-        else if (userDB.checkIfUserExists(user.email) == STATUS_USER_CREATED){
+        } else if (userDB.checkIfUserExists(user.email) == STATUS_USER_FOUND) {
             System.out.println("Email already used");
             //front end stuff
         }
         return STATUS_OK;
     }
 
+    public Status logIn(User user) {
+        return STATUS_OK;
+    }
 }

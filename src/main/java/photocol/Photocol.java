afc737ce2401a4d25d3e05d9c9cd36cc9d2c8bf4
Jonathan Lam <jlam55555@gmail.com>
2020-05-06 17:20:28 -0400

    cleaned up imports in photocol

--- src/main/java/photocol/Photocol.java
+++ src/main/java/photocol/Photocol.java
@@ -1,24 +1,14 @@
 /* Main class for the Photocol app */
-
 package photocol;
 
 import com.google.gson.Gson;
-import photocol.layer.handler.SearchHandler;
-import photocol.layer.store.UserStore;
-import photocol.layer.handler.CollectionHandler;
-import photocol.layer.handler.PhotoHandler;
-import photocol.layer.handler.UserHandler;
-import photocol.layer.service.CollectionService;
-import photocol.layer.service.PhotoService;
-import photocol.layer.service.UserService;
-import photocol.layer.store.CollectionStore;
-import photocol.layer.store.PhotoStore;
-import photocol.util.DBConnectionClient;
-import photocol.util.S3ConnectionClient;
+import photocol.layer.handler.*;
+import photocol.layer.service.*;
+import photocol.layer.store.*;
+import photocol.util.*;
 
 public class Photocol {
     public static void main(String[] args) {
-
         // services
         Gson gson = new Gson();
         S3ConnectionClient s3 = new S3ConnectionClient();
